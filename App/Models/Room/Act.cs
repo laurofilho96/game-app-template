34f97a9c5a89d66c90e04ca364e6e358a47d4961
Lauro Vieira <laurofilho96@gmail.com>
2026-04-20 16:19:27 -0300

    Agora sim

---
+++ App/Models/Room/Act.cs
@@ -0,0 +1,14 @@
+﻿using Models.Characters;
+
+namespace Models.Room;
+
+public class Act
+{
+    public Guid ActId { get; set; }
+    public required string ActName { get; set; }
+    public int ActNumber { get; set; }
+    public string? ActDescription { get; set; }
+    public string? ActResult { get; set; }
+    public List<Enemy>? ActEnemies { get; set; }
+    public List<Character> Characters { get; set; }
+}
